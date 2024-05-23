138520a4ca800a18d7dca9c130ddfeb58f063b8c
jerry.aaron <jerry.aaron@vfdtech.ng>
2024-05-23 10:53:10 +0100

    admin pages

---
+++ src/routes/admin.tsx
@@ -0,0 +1,27 @@
+import { AdminRoutes, RouteEnum } from "constants/RouterConstants";
+
+import { lazy } from "react";
+import { Navigate } from "react-router-dom";
+
+export const adminRoutes = [
+  {
+    path: "*",
+    element: <Navigate to={RouteEnum.DASHBOARD} />,
+  },
+  {
+    path: AdminRoutes.OVERVIEW,
+    element: lazy(() => import("pages/protectedPages/admin/Overview")),
+  },
+  {
+    path: AdminRoutes.CONSUMABLES,
+    element: lazy(() => import("pages/protectedPages/admin/Consumables")),
+  },
+  {
+    path: AdminRoutes.ASSETS,
+    element: lazy(() => import("pages/protectedPages/admin/Assets")),
+  },
+  {
+    path: AdminRoutes.CREateConsumables,
+    element: lazy(() => import("pages/protectedPages/admin/CreateConsumables")),
+  },
+];
