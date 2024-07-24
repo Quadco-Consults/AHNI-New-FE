15a4c11e167bb779594e6dd920ffd48f0ea20fee
yunusakenchi <yunusakenchi@gmail.com>
2024-07-24 18:51:08 +0100

    Integrated API

--- src/pages/protectedPages/modules/programs/Facility.tsx
+++ src/pages/protectedPages/modules/programs/Facility.tsx
@@ -1,3 +1,12 @@
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "components/ui/dropdown-menu";
+import { Edit, RefreshCw } from "lucide-react";
 import { Button } from "components/ui/button";
 import AddFacility from "./AddFacility";
 import {
@@ -6,9 +15,16 @@ import {
   DialogTrigger,
   DialogTitle,
 } from "components/ui/dialog";
-import { FACILITY } from "./list";
+import { ChevronDown } from "lucide-react";
+import { useFacilitiesQuery } from "services/module-programs";
 
 const Facility = () => {
+  const { data } = useFacilitiesQuery({
+    no_paginate: false,
+  });
+
+
+
   return (
     <div>
       <div className="flex justify-between items-center py-6 mb-6">
@@ -32,28 +48,53 @@ const Facility = () => {
         </Dialog>
       </div>
       <div>
-        <div className="flex gap-[6.5rem] text-[#756D6D] font-semibold text-sm border-b border-gray-300 pb-4">
-          <h1>Facility Name</h1>
+        <div className="flex gap-[5rem] text-[#756D6D] font-semibold text-sm border-b border-gray-300 pb-4">
+          <h1 className="">Facility Name</h1>
           <h1>Contact Person</h1>
-          <h1 className="ml-[-2.8rem]">Position</h1>
-          <h1 className="ml-[3.5rem]">Phone Number</h1>
-          <h1 className="ml-[-2rem]">State</h1>
-          <h1 className="ml-[-2rem]">LGA</h1>
-          {/* <h1></h1> */}
+          <h1 className="">Position</h1>
+          <h1>Phone Number</h1>
+          <h1>Email</h1>
+          <h1 className="">State</h1>
+          <h1>LGA</h1>
+          <h1></h1>
         </div>
         <div>
-          {FACILITY.map((table, index) => (
+          {data?.map((item) => (
             <div
-              key={index}
+              key={item.id}
               className="flex justify-between mt-6 text-[#756D6D] font-normal text-xs"
             >
-              <p>{table.facilityName}</p>
-              <p>{table.contactPerson}</p>
-              <p>{table.position}</p>
-              <p>{table.phoneNumber}</p>
-              <p>{table.state}</p>
-              <p>{table.lga}</p>
-              <img src={table.icon} className="w-[20px] object-contain" alt="" />
+              <p>{item.name}</p>
+              {item.facility_contacts.map((contact) => (
+                <div key={contact.id} className="flex gap-[4rem]">
+                  <p>{contact.name}</p>
+                  <p>{contact.position}</p>
+                  <p>{contact.phone_number}</p>
+                  <p>{contact.email}</p>
+                </div>
+              ))}
+              <p>{item.state}</p>
+              <p>{item.local_govt}</p>
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                  <Button variant="ghost" className="w-8 h-8 p-0">
+                    <span className="sr-only">Open menu</span>
+                    <ChevronDown className="w-4 h-4" />
+                  </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent align="end">
+                  <DropdownMenuLabel>Actions</DropdownMenuLabel>
+                  <DropdownMenuSeparator />
+                  <DropdownMenuItem className="cursor-pointer ">
+                    <Edit className="w-4 h-4 mr-2" />
+                    <span>Edit</span>
+                  </DropdownMenuItem>
+                  <DropdownMenuItem className="cursor-pointer ">
+                    <RefreshCw className="w-4 h-4 mr-2" />
+                    <span>Update Role</span>
+                  </DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
             </div>
           ))}
         </div>
