b0bee732796eefd9a4bbb7984264bde321ab7f06
thecoderhoc <ubakawilson@gmail.com>
2024-11-09 17:59:39 +0100

    Updated the authentication pages with the new design

--- src/definations/auth.ts
+++ src/definations/auth.ts
@@ -1,69 +1,69 @@
 import { z } from "zod";
 
 export const loginSchema = z.object({
-  email: z.string().email(),
-  password: z.string(),
+    email: z.string().email().min(1, "Please enter your email"),
+    password: z.string().min(1, "Please enter your password"),
 });
 
 export type LoginData = {
-  access_token: string;
-  refresh_token: string;
-  user: {
-    id: string;
-    first_name: string;
-    last_name: string;
-    email: string;
-    last_login: string;
-    roles: string[];
-    permissions: string[];
-  };
+    access_token: string;
+    refresh_token: string;
+    user: {
+        id: string;
+        first_name: string;
+        last_name: string;
+        email: string;
+        last_login: string;
+        roles: string[];
+        permissions: string[];
+    };
 };
 
 export interface LoginResponse {
-  message: string;
-  data: LoginData;
+    message: string;
+    data: LoginData;
 }
 
 export interface TRequest {
-  page?: number;
-  page_size?: number;
-  no_paginate?: boolean;
-  fields?: string;
-  consumable?: string;
-  id?: string;
-  classification?: string;
-  vehicle?: string;
-  asset_type?: string;
+    page?: number;
+    page_size?: number;
+    no_paginate?: boolean;
+    fields?: string;
+    consumable?: string;
+    id?: string;
+    classification?: string;
+    vehicle?: string;
+    asset_type?: string;
 }
 
 export interface TBasePaginatedResponse<T> {
-  count: number;
-  next: string;
-  previous: string;
-  results: T;
+    count: number;
+    next: string;
+    previous: string;
+    results: T;
 }
 export interface TBaseCreateResponse<T> {
-  message: string;
-  data: T;
+    message: string;
+    data: T;
 }
 
 export interface TFacilityResponse<T> {
-  id: string;
-  name: string;
-  state: string;
-  local_govt: string;
-  facility_contacts: T;
+    id: string;
+    name: string;
+    state: string;
+    local_govt: string;
+    facility_contacts: T;
 }
 export type TFacilityResponseArray<T> = TFacilityResponse<T>[];
 
 export interface TSupervisionCategoryResponse {
-  id: string;
-  code: string;
-  created_at: string;
-  updated_at: string;
-  name: string;
-  description: string;
-  serial_number: number;
-  job_category: string;
+    id: string;
+    code: string;
+    created_at: string;
+    updated_at: string;
+    name: string;
+    description: string;
+    serial_number: number;
+    job_category: string;
 }
 export type TSupervisionCategoryResponseArray = TSupervisionCategoryResponse[];
--- src/pages/Auth/Login.tsx
+++ src/pages/Auth/Login.tsx
@@ -3,15 +3,16 @@ import LoginForm from "molecules/forms/LoginForm";
 export default function Login() {
     return (
         <div className="flex flex-1 h-screen">
-            <div className="flex bg-gray-50 w-full">
+            <div className="flex bg-[#FEF2F2] w-full">
                 <div className="w-full flex flex-1 items-center justify-center">
                     <LoginForm />
                 </div>
-                <div className="hidden md:flex md:flex-1 ">
-                    <img
-                        src="/imgs/LoginImage.png"
-                        className="flex flex-1 h-screen"
-                    />
+                <div className="hidden md:flex md:flex-1 md:items-center md:justify-center bg-[url('/img/login.jpeg')] bg-[#161630B2] bg-blend-overlay bg-opacity-100">
+                    <div className="font-bold text-xl text-white text-center">
+                        Providing solutions that are essential to the <br />
+                        advancement of human development in communities we{" "}
+                        <br /> serve
+                    </div>
                 </div>
             </div>
         </div>
