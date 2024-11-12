c2bf7fa465e99be91a1e4c649ff4a643290d767f
thecoderhoc <ubakawilson@gmail.com>
2024-11-12 08:48:00 +0100

    CORS error fix test

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,89 +1,89 @@
 import {
-  BaseQueryFn,
-  FetchArgs,
-  fetchBaseQuery,
-  FetchBaseQueryError,
-  createApi,
+    BaseQueryFn,
+    FetchArgs,
+    fetchBaseQuery,
+    FetchBaseQueryError,
+    createApi,
 } from "@reduxjs/toolkit/query/react";
 import { RootState } from "src/store";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "https://apierp.ahnigeria.org/api/v1/",
-  prepareHeaders: (headers, { getState }) => {
-    const { auth } = getState() as RootState;
+    baseUrl: "https://ahni-erp-029252c2fbb9.herokuapp.com/api/v1/",
+    prepareHeaders: (headers, { getState }) => {
+        const { auth } = getState() as RootState;
 
-    if (auth.access_token) {
-      headers.set("Authorization", `Bearer ${auth.access_token}`);
-    }
-    return headers;
-  },
+        if (auth.access_token) {
+            headers.set("Authorization", `Bearer ${auth.access_token}`);
+        }
+        return headers;
+    },
 });
 const baseQueryWithReauth: BaseQueryFn<
-  string | FetchArgs,
-  unknown,
-  FetchBaseQueryError
+    string | FetchArgs,
+    unknown,
+    FetchBaseQueryError
 > = async (args, api, extraOptions) => {
-  const result = await baseQuery(args, api, extraOptions);
+    const result = await baseQuery(args, api, extraOptions);
 
-  if (result.error && result.error.status === 404) {
-    return result.error;
-  }
-  return result;
+    if (result.error && result.error.status === 404) {
+        return result.error;
+    }
+    return result;
 };
 
 const baseAPI = createApi({
-  reducerPath: "baseApi",
-  baseQuery: baseQueryWithReauth,
-  endpoints: () => ({}),
-  //  cache , The default time is seconds , Default duration 60 second
+    reducerPath: "baseApi",
+    baseQuery: baseQueryWithReauth,
+    endpoints: () => ({}),
+    //  cache , The default time is seconds , Default duration 60 second
 
-  tagTypes: [
-    "Users",
-    "Permission",
-    "FundingSource",
-    "Beneficiaries",
-    "DocumentTypes",
-    "Partners",
-    "Facilities",
-    "SupervisionCategory",
-    "RiskCategory",
-    "Stock",
-    "FUND_REQUEST",
-    "PURCHASE_REQUEST",
-    "ITEMS",
-    "PURCHASE_ORDER",
-    "SOLICITATION",
-    "SOLICITATION_CRITERIA",
-    "CBA",
-    "PRICE_INTELLIGENCE",
-    "Agreement",
-    "Facility",
-    "FuelRecord",
-    "VehicleRequest",
-    "PROCUREMENT_PLAN",
-    "PROCUREMENT_TRACKER",
-    "PAYMENT",
-    "AM",
-    "AssetsRequest",
-    "AssetConditions",
-    "AssetTypes",
-    "Categories",
-    "Departments",
-    "FinancialYear",
-    "Items",
-    "Locations",
-    "Lots",
-    "Solicitation",
-    "PrequalificationCategory",
-    "PrequalificationCriteria",
-    "Questionairs",
-    "WORKFORCE",
-    "HR_GRADE",
-    "HR_POSITION",
-    "HR_BENEFICIARIES",
-  ],
-  keepUnusedDataFor: 5 * 60,
-  refetchOnMountOrArgChange: true,
+    tagTypes: [
+        "Users",
+        "Permission",
+        "FundingSource",
+        "Beneficiaries",
+        "DocumentTypes",
+        "Partners",
+        "Facilities",
+        "SupervisionCategory",
+        "RiskCategory",
+        "Stock",
+        "FUND_REQUEST",
+        "PURCHASE_REQUEST",
+        "ITEMS",
+        "PURCHASE_ORDER",
+        "SOLICITATION",
+        "SOLICITATION_CRITERIA",
+        "CBA",
+        "PRICE_INTELLIGENCE",
+        "Agreement",
+        "Facility",
+        "FuelRecord",
+        "VehicleRequest",
+        "PROCUREMENT_PLAN",
+        "PROCUREMENT_TRACKER",
+        "PAYMENT",
+        "AM",
+        "AssetsRequest",
+        "AssetConditions",
+        "AssetTypes",
+        "Categories",
+        "Departments",
+        "FinancialYear",
+        "Items",
+        "Locations",
+        "Lots",
+        "Solicitation",
+        "PrequalificationCategory",
+        "PrequalificationCriteria",
+        "Questionairs",
+        "WORKFORCE",
+        "HR_GRADE",
+        "HR_POSITION",
+        "HR_BENEFICIARIES",
+    ],
+    keepUnusedDataFor: 5 * 60,
+    refetchOnMountOrArgChange: true,
 });
 
 export default baseAPI;
