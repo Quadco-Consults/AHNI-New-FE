a719447646e105b3c922d0d06b9263714d11e603
thecoderhoc <ubakawilson@gmail.com>
2024-11-13 22:42:32 +0100

    New UI Created

--- src/pages/protectedPages/programs/stakeholder-management/register/[id]/index.tsx
+++ src/pages/protectedPages/programs/stakeholder-management/register/[id]/index.tsx
@@ -5,69 +5,106 @@ import { useNavigate, useParams } from "react-router-dom";
 import StakeholderManagementAPI from "services/programsApi/stakeholder-management";
 
 const RegisterDetails = () => {
-  const navigate = useNavigate();
-  const { id } = useParams();
+    const navigate = useNavigate();
+    const { id } = useParams();
 
-  const { data, isLoading } =
-    StakeholderManagementAPI.useGetStakeholderManagementQuery({
-      path: { id: id as string },
-    });
+    const { data, isLoading } =
+        StakeholderManagementAPI.useGetStakeholderManagementQuery({
+            path: { id: id as string },
+        });
 
-  const goBack = () => {
-    navigate(-1);
-  };
+    const goBack = () => {
+        navigate(-1);
+    };
 
-  return (
-    <div className="space-y-6 min-h-screen">
-      <button
-        onClick={goBack}
-        className="w-[3rem] aspect-square rounded-full drop-shadow-md bg-white flex items-center justify-center"
-      >
-        <LongArrowLeft />
-      </button>
+    return (
+        <div className="space-y-6 min-h-screen">
+            <button
+                onClick={goBack}
+                className="w-[3rem] aspect-square rounded-full drop-shadow-md bg-white flex items-center justify-center"
+            >
+                <LongArrowLeft />
+            </button>
 
-      <Card className="space-y-6">
-        {isLoading && <LoadingSpinner />}
-        <h4 className="font-semibold">
-          {data?.stake_holder?.stakeholder_name}
-        </h4>
-        <p className="font-extralight">ACEBAY</p>
+            <Card className="space-y-6">
+                {isLoading && <LoadingSpinner />}
+                <h4 className="font-semibold">
+                    {data?.stake_holder?.stakeholder_name}
+                </h4>
+                <p className="font-extralight">ACEBAY</p>
 
-        <div className="grid grid-cols-2 gap-3">
-          <div>
-            <h4 className="font-semibold">Institution/Organization:</h4>
-            <p>{data?.stake_holder?.institution_organization}</p>
-          </div>
-          <div>
-            <h4 className="font-semibold">Physical Office Address:</h4>
-            <p>{data?.stake_holder?.physical_office_address}</p>
-          </div>
+                <div className="grid grid-cols-2 gap-3">
+                    <div>
+                        <h4 className="font-semibold">
+                            Institution/Organization:
+                        </h4>
+                        <p>{data?.stake_holder?.institution_organization}</p>
+                    </div>
+                    <div>
+                        <h4 className="font-semibold">
+                            Physical Office Address:
+                        </h4>
+                        <p>{data?.stake_holder?.physical_office_address}</p>
+                    </div>
+                </div>
+
+                <div className="grid grid-cols-2 gap-3">
+                    <div>
+                        <h4 className="font-semibold">State:</h4>
+                        <p>{data?.stake_holder?.state}</p>
+                    </div>
+                    <div>
+                        <h4 className="font-semibold">Designation:</h4>
+                        <p>{data?.stake_holder?.designation}</p>
+                    </div>
+                </div>
+
+                <div className="grid grid-cols-2 gap-3">
+                    <div>
+                        <h4 className="font-semibold">Phone Number:</h4>
+                        <p>{data?.stake_holder?.phone_number}</p>
+                    </div>
+                    <div>
+                        <h4 className="font-semibold">E-mail:</h4>
+                        <p>{data?.stake_holder?.email}</p>
+                    </div>
+                </div>
+
+                <div className="grid grid-cols-2 gap-3">
+                    <div>
+                        <h4 className="font-semibold">Project Role:</h4>
+                        <p>{data?.stake_holder?.project_role}</p>
+                    </div>
+                    <div>
+                        <h4 className="font-semibold">Importance:</h4>
+                        <p>{data?.stake_holder?.importance}</p>
+                    </div>
+                </div>
+
+                <div className="grid grid-cols-2 gap-3">
+                    <div>
+                        <h4 className="font-semibold">Influence:</h4>
+                        <p>{data?.stake_holder?.influence}</p>
+                    </div>
+                    <div>
+                        <h4 className="font-semibold">Score:</h4>
+                        <p>{data?.stake_holder?.score}</p>
+                    </div>
+                </div>
+
+                <div className="grid grid-cols-2 gap-3">
+                    <div>
+                        <h4 className="font-semibold">Major Concerns:</h4>
+                        <p>{data?.stake_holder?.major_concerns}</p>
+                    </div>
+                    <div>
+                        <h4 className="font-semibold">Relationship Owner:</h4>
+                        <p>{data?.stake_holder?.relationship_owner}</p>
+                    </div>
+                </div>
+            </Card>
         </div>
-
-        <div className="grid grid-cols-2 gap-3">
-          <div>
-            <h4 className="font-semibold">State:</h4>
-            <p>{data?.stake_holder?.state}</p>
-          </div>
-          <div>
-            <h4 className="font-semibold">Designation:</h4>
-            <p>{data?.stake_holder?.designation}</p>
-          </div>
-        </div>
-
-        <div className="grid grid-cols-2 gap-3">
-          <div>
-            <h4 className="font-semibold">Phone Number:</h4>
-            <p>{data?.stake_holder?.phone_number}</p>
-          </div>
-          <div>
-            <h4 className="font-semibold">E-mail:</h4>
-            <p>{data?.stake_holder?.email}</p>
-          </div>
-        </div>
-      </Card>
-    </div>
-  );
+    );
 };
 
 export default RegisterDetails;
--- src/routes/program.tsx
+++ src/routes/program.tsx
@@ -3,195 +3,231 @@ import { RouteEnum } from "constants/RouterConstants";
 import { lazy } from "react";
 
 export const program = [
-  {
-    path: RouteEnum.PROGRAM_WORK_PLAN,
-    element: lazy(
-      () => import("pages/protectedPages/programs/plan/work-plan/index")
-    ),
-  },
-  {
-    path: RouteEnum.PROGRAM_WORK_PLAN_DETAILS,
-    element: lazy(
-      () => import("pages/protectedPages/programs/plan/work-plan/id/index")
-    ),
-  },
-  {
-    path: RouteEnum.PROGRAM_ACTIVITY,
-    element: lazy(
-      () => import("pages/protectedPages/programs/plan/activity-plan/index")
-    ),
-  },
-  {
-    path: RouteEnum.PROGRAM_SUPPORTIVE_SUPERVISION,
-    element: lazy(() => import("pages/protectedPages/programs/plan/ssp/index")),
-  },
-  {
-    path: RouteEnum.PROGRAM_SUPPORTIVE_SUPERVISION_DETAILS,
-    element: lazy(
-      () => import("pages/protectedPages/programs/plan/ssp/[id]/index")
-    ),
-  },
-  {
-    path: RouteEnum.PROGRAM_SUPPORTIVE_SUPERVISION_MANAGEMENT,
-    element: lazy(
-      () =>
-        import(
-          "pages/protectedPages/programs/plan/ssp/[id]/CoreManagementSystems"
-        )
-    ),
-  },
-  {
-    path: RouteEnum.PROGRAM_SUPPORTIVE_SUPERVISION_DETAILS_APPROVAL,
-    element: lazy(
-      () => import("pages/protectedPages/programs/plan/ssp/[id]/ApprovalStatus")
-    ),
-  },
-  {
-    path: RouteEnum.PROGRAM_SUPPORTIVE_SUPERVISION_COMPOSITION,
-    element: lazy(
-      () => import("pages/protectedPages/programs/plan/ssp/Composition")
-    ),
-  },
-  {
-    path: RouteEnum.PROGRAM_SUPPORTIVE_SUPERVISION_CHECKLIST,
-    element: lazy(
-      () => import("pages/protectedPages/programs/plan/ssp/Checklist")
-    ),
-  },
-  {
-    path: RouteEnum.PROGRAM_FUND_REQUEST,
-    element: lazy(
-      () => import("pages/protectedPages/programs/fund-request/index")
-    ),
-  },
-  {
-    path: RouteEnum.PROGRAM_FUND_REQUEST_DETAILS,
-    element: lazy(
-      () => import("pages/protectedPages/programs/fund-request/id/index")
-    ),
-  },
-  {
-    path: RouteEnum.PROGRAM_FUND_REQUEST_CREATE,
-    element: lazy(
-      () =>
-        import("pages/protectedPages/programs/fund-request/Create-fund-request")
-    ),
-  },
-  {
-    path: RouteEnum.PROGRAM_FUND_REQUEST_PREVIEW,
-    element: lazy(
-      () => import("pages/protectedPages/programs/fund-request/Fund-request-preview")
-    ),
-  },
-  {
-    path: RouteEnum.PROGRAM_FUND_REQUEST_FUND_SUMMARY,
-    element: lazy(
-      () => import("pages/protectedPages/programs/fund-request/Fund-summary")
-    ),
-  },
-  {
-    path: RouteEnum.PROGRAM_RISK_MANAGEMENT,
-    element: lazy(
-      () => import("pages/protectedPages/programs/plan/risk-management/index")
-    ),
-  },
-  {
-    path: RouteEnum.PROGRAM_RISK_MANAGEMENT_CREATE,
-    element: lazy(
-      () =>
-        import(
-          "pages/protectedPages/programs/plan/risk-management/Create-risk-management"
-        )
-    ),
-  },
-  {
-    path: RouteEnum.PROGRAM_STAKEHOLDER_MANAGEMENT_ANALYSIS,
-    element: lazy(
-      () =>
-        import(
-          "pages/protectedPages/programs/stakeholder-management/analysis-mapping/index"
-        )
-    ),
-  },
-  {
-    path: RouteEnum.PROGRAM_STAKEHOLDER_MANAGEMENT_ANALYSIS_CREATE,
-    element: lazy(
-      () =>
-        import(
-          "pages/protectedPages/programs/stakeholder-management/analysis-mapping/Create-analysis"
-        )
-    ),
-  },
-  {
-    path: RouteEnum.PROGRAM_STAKEHOLDER_MANAGEMENT_ANALYSIS_DETAILS,
-    element: lazy(
-      () =>
-        import(
-          "pages/protectedPages/programs/stakeholder-management/analysis-mapping/[id]/index"
-        )
-    ),
-  },
-  {
-    path: RouteEnum.PROGRAM_STAKEHOLDER_MANAGEMENT_REGISTER,
-    element: lazy(
-      () =>
-        import(
-          "pages/protectedPages/programs/stakeholder-management/register/index"
-        )
-    ),
-  },
-  {
-    path: RouteEnum.PROGRAM_STAKEHOLDER_MANAGEMENT_REGISTER_CREATE,
-    element: lazy(
-      () =>
-        import(
-          "pages/protectedPages/programs/stakeholder-management/register/Create-register"
-        )
-    ),
-  },
-  {
-    path: RouteEnum.PROGRAM_STAKEHOLDER_MANAGEMENT_REGISTER_EDIT,
-    element: lazy(
-      () =>
-        import(
-          "pages/protectedPages/programs/stakeholder-management/register/edit-register"
-        )
-    ),
-  },
-  {
-    path: RouteEnum.PROGRAM_STAKEHOLDER_MANAGEMENT_REGISTER_DETAILS,
-    element: lazy(
-      () =>
-        import(
-          "pages/protectedPages/programs/stakeholder-management/register/[id]/index"
-        )
-    ),
-  },
-  {
-    path: RouteEnum.PROGRAM_STAKEHOLDER_MANAGEMENT_PLAN,
-    element: lazy(
-      () =>
-        import(
-          "pages/protectedPages/programs/stakeholder-management/engagement/index"
-        )
-    ),
-  },
-  {
-    path: RouteEnum.PROGRAM_STAKEHOLDER_MANAGEMENT_PLAN_CREATE,
-    element: lazy(
-      () =>
-        import(
-          "pages/protectedPages/programs/stakeholder-management/engagement/Create-engagement"
-        )
-    ),
-  },
-  {
-    path: RouteEnum.PROGRAM_STAKEHOLDER_MANAGEMENT_PLAN_DETAILS,
-    element: lazy(
-      () =>
-        import(
-          "pages/protectedPages/programs/stakeholder-management/engagement/[id]/index"
-        )
-    ),
-  },
+    {
+        path: RouteEnum.PROGRAM_WORK_PLAN,
+        element: lazy(
+            () => import("pages/protectedPages/programs/plan/work-plan/index")
+        ),
+    },
+    {
+        path: RouteEnum.PROGRAM_WORK_PLAN_DETAILS,
+        element: lazy(
+            () =>
+                import("pages/protectedPages/programs/plan/work-plan/id/index")
+        ),
+    },
+    {
+        path: RouteEnum.PROGRAM_ACTIVITY,
+        element: lazy(
+            () =>
+                import("pages/protectedPages/programs/plan/activity-plan/index")
+        ),
+    },
+    {
+        path: RouteEnum.PROGRAM_ACTIVITY_TRACKER,
+        element: lazy(
+            () =>
+                import(
+                    "pages/protectedPages/programs/plan/activity-tracker/index"
+                )
+        ),
+    },
+    {
+        path: RouteEnum.PROGRAM_ACTIVITY_TRACKER_CREATE,
+        element: lazy(
+            () =>
+                import(
+                    "pages/protectedPages/programs/plan/activity-tracker/create-activity-tracker"
+                )
+        ),
+    },
+    {
+        path: RouteEnum.PROGRAM_SUPPORTIVE_SUPERVISION,
+        element: lazy(
+            () => import("pages/protectedPages/programs/plan/ssp/index")
+        ),
+    },
+    {
+        path: RouteEnum.PROGRAM_SUPPORTIVE_SUPERVISION_DETAILS,
+        element: lazy(
+            () => import("pages/protectedPages/programs/plan/ssp/[id]/index")
+        ),
+    },
+    {
+        path: RouteEnum.PROGRAM_SUPPORTIVE_SUPERVISION_MANAGEMENT,
+        element: lazy(
+            () =>
+                import(
+                    "pages/protectedPages/programs/plan/ssp/[id]/CoreManagementSystems"
+                )
+        ),
+    },
+    {
+        path: RouteEnum.PROGRAM_SUPPORTIVE_SUPERVISION_DETAILS_APPROVAL,
+        element: lazy(
+            () =>
+                import(
+                    "pages/protectedPages/programs/plan/ssp/[id]/ApprovalStatus"
+                )
+        ),
+    },
+    {
+        path: RouteEnum.PROGRAM_SUPPORTIVE_SUPERVISION_COMPOSITION,
+        element: lazy(
+            () => import("pages/protectedPages/programs/plan/ssp/Composition")
+        ),
+    },
+    {
+        path: RouteEnum.PROGRAM_SUPPORTIVE_SUPERVISION_CHECKLIST,
+        element: lazy(
+            () => import("pages/protectedPages/programs/plan/ssp/Checklist")
+        ),
+    },
+    {
+        path: RouteEnum.PROGRAM_FUND_REQUEST,
+        element: lazy(
+            () => import("pages/protectedPages/programs/fund-request/index")
+        ),
+    },
+    {
+        path: RouteEnum.PROGRAM_FUND_REQUEST_DETAILS,
+        element: lazy(
+            () => import("pages/protectedPages/programs/fund-request/id/index")
+        ),
+    },
+    {
+        path: RouteEnum.PROGRAM_FUND_REQUEST_CREATE,
+        element: lazy(
+            () =>
+                import(
+                    "pages/protectedPages/programs/fund-request/Create-fund-request"
+                )
+        ),
+    },
+    {
+        path: RouteEnum.PROGRAM_FUND_REQUEST_PREVIEW,
+        element: lazy(
+            () =>
+                import(
+                    "pages/protectedPages/programs/fund-request/Fund-request-preview"
+                )
+        ),
+    },
+    {
+        path: RouteEnum.PROGRAM_FUND_REQUEST_FUND_SUMMARY,
+        element: lazy(
+            () =>
+                import(
+                    "pages/protectedPages/programs/fund-request/Fund-summary"
+                )
+        ),
+    },
+    {
+        path: RouteEnum.PROGRAM_RISK_MANAGEMENT,
+        element: lazy(
+            () =>
+                import(
+                    "pages/protectedPages/programs/plan/risk-management/index"
+                )
+        ),
+    },
+    {
+        path: RouteEnum.PROGRAM_RISK_MANAGEMENT_CREATE,
+        element: lazy(
+            () =>
+                import(
+                    "pages/protectedPages/programs/plan/risk-management/Create-risk-management"
+                )
+        ),
+    },
+    {
+        path: RouteEnum.PROGRAM_STAKEHOLDER_MANAGEMENT_ANALYSIS,
+        element: lazy(
+            () =>
+                import(
+                    "pages/protectedPages/programs/stakeholder-management/analysis-mapping/index"
+                )
+        ),
+    },
+    {
+        path: RouteEnum.PROGRAM_STAKEHOLDER_MANAGEMENT_ANALYSIS_CREATE,
+        element: lazy(
+            () =>
+                import(
+                    "pages/protectedPages/programs/stakeholder-management/analysis-mapping/Create-analysis"
+                )
+        ),
+    },
+    {
+        path: RouteEnum.PROGRAM_STAKEHOLDER_MANAGEMENT_ANALYSIS_DETAILS,
+        element: lazy(
+            () =>
+                import(
+                    "pages/protectedPages/programs/stakeholder-management/analysis-mapping/[id]/index"
+                )
+        ),
+    },
+    {
+        path: RouteEnum.PROGRAM_STAKEHOLDER_MANAGEMENT_REGISTER,
+        element: lazy(
+            () =>
+                import(
+                    "pages/protectedPages/programs/stakeholder-management/register/index"
+                )
+        ),
+    },
+    {
+        path: RouteEnum.PROGRAM_STAKEHOLDER_MANAGEMENT_REGISTER_CREATE,
+        element: lazy(
+            () =>
+                import(
+                    "pages/protectedPages/programs/stakeholder-management/register/Create-register"
+                )
+        ),
+    },
+    {
+        path: RouteEnum.PROGRAM_STAKEHOLDER_MANAGEMENT_REGISTER_EDIT,
+        element: lazy(
+            () =>
+                import(
+                    "pages/protectedPages/programs/stakeholder-management/register/edit-register"
+                )
+        ),
+    },
+    {
+        path: RouteEnum.PROGRAM_STAKEHOLDER_MANAGEMENT_REGISTER_DETAILS,
+        element: lazy(
+            () =>
+                import(
+                    "pages/protectedPages/programs/stakeholder-management/register/[id]/index"
+                )
+        ),
+    },
+    {
+        path: RouteEnum.PROGRAM_STAKEHOLDER_MANAGEMENT_PLAN,
+        element: lazy(
+            () =>
+                import(
+                    "pages/protectedPages/programs/stakeholder-management/engagement/index"
+                )
+        ),
+    },
+    {
+        path: RouteEnum.PROGRAM_STAKEHOLDER_MANAGEMENT_PLAN_CREATE,
+        element: lazy(
+            () =>
+                import(
+                    "pages/protectedPages/programs/stakeholder-management/engagement/Create-engagement"
+                )
+        ),
+    },
+    {
+        path: RouteEnum.PROGRAM_STAKEHOLDER_MANAGEMENT_PLAN_DETAILS,
+        element: lazy(
+            () =>
+                import(
+                    "pages/protectedPages/programs/stakeholder-management/engagement/[id]/index"
+                )
+        ),
+    },
 ];
