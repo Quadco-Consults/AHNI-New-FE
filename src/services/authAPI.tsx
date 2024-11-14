e334000d4516f57bb12e19ce322c85aba73c1dba
thecoderhoc <ubakawilson@gmail.com>
2024-11-14 09:38:02 +0100

    Implemented forgot and change password

--- src/services/authAPI.tsx
+++ src/services/authAPI.tsx
@@ -3,40 +3,49 @@ import baseAPI from ".";
 import { LoginResponse, loginSchema } from "definations/auth";
 
 const authAPi = baseAPI.injectEndpoints({
-  endpoints: (builder) => ({
-    login: builder.mutation<LoginResponse, z.infer<typeof loginSchema>>({
-      query: (body) => ({
-        url: "/auth/login/",
-        method: "POST",
-        body,
-      }),
+    endpoints: (builder) => ({
+        login: builder.mutation<LoginResponse, z.infer<typeof loginSchema>>({
+            query: (body) => ({
+                url: "/auth/login/",
+                method: "POST",
+                body,
+            }),
+        }),
+        forgotPassword: builder.mutation<any, { email: string }>({
+            query: (body) => ({
+                url: "/auth/forgot_password/",
+                method: "POST",
+                body,
+            }),
+        }),
+        verifyOTP: builder.mutation<any, { email: string; token: string }>({
+            query: (body) => ({
+                method: "POST",
+                url: "/auth/verify-reset-token/",
+                body,
+            }),
+        }),
+        changePassword: builder.mutation<
+            any,
+            {
+                email: string | null;
+                token: string;
+                new_password: string;
+                confirm_password: string;
+            }
+        >({
+            query: (body) => ({
+                url: "/auth/reset_password/",
+                method: "POST",
+                body,
+            }),
+        }),
     }),
-    forgotPassword: builder.mutation<any, { email: string }>({
-      query: (body) => ({
-        url: "/auth/forgot_password/",
-        method: "POST",
-        body,
-      }),
-    }),
-    changePassword: builder.mutation<
-      any,
-      {
-        // old_password: string;
-        new_password: string;
-        confirm_new_password: string;
-      }
-    >({
-      query: (body) => ({
-        url: "/auth/change_password/",
-        method: "POST",
-        body,
-      }),
-    }),
-  }),
 });
 
 export const {
-  useLoginMutation,
-  useChangePasswordMutation,
-  useForgotPasswordMutation,
+    useLoginMutation,
+    useChangePasswordMutation,
+    useForgotPasswordMutation,
+    useVerifyOTPMutation,
 } = authAPi;
