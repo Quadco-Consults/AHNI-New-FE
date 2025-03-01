fb956ee43b5d3260e2c6bc16ca7e40185901cefc
Ifeanyi Ejindu <ifeanyiejindu@gmail.com>
2025-03-01 09:17:37 +0100

    Ifeanyi done come

--- src/components/Table/columns/c&g/sub-grant/awards.tsx
+++ src/components/Table/columns/c&g/sub-grant/awards.tsx
@@ -12,138 +12,142 @@ import { CG_ROUTES } from "constants/RouterConstants";
 import { toast } from "sonner";
 import { useDeleteSubGrantMutation } from "services/c&g/subgrant/sub-grant";
 import ConfirmationDialog from "components/modals/dailog/ConfirmationDialog";
+import { formatNumberCurrency } from "utils/utls";
 
 export const subGrantAwardColumns: ColumnDef<ISubGrantPaginatedData>[] = [
-    {
-        header: "Title",
-        id: "title",
-        accessorKey: "title",
-        size: 200,
-    },
+  {
+    header: "Title",
+    id: "title",
+    accessorKey: "title",
+    size: 200,
+  },
 
-    {
-        header: "Project",
-        id: "project",
-        accessorKey: "project",
-        size: 200,
-    },
+  {
+    header: "Project",
+    id: "project",
+    accessorKey: "project",
+    size: 200,
+  },
 
-    {
-        header: "Business Unit",
-        id: "business_unit",
-        accessorKey: "business_unit",
-        size: 200,
-    },
+  {
+    header: "Business Unit",
+    id: "business_unit",
+    accessorKey: "business_unit",
+    size: 200,
+  },
 
-    {
-        header: "Award Amount (USD)",
-        id: "amount_usd",
-        accessorKey: "amount_usd",
-        size: 200,
-    },
+  {
+    header: "Award Amount (USD)",
+    id: "amount_usd",
+    accessorFn: (data) => formatNumberCurrency(data.amount_usd, "USD"),
+    accessorKey: "amount_usd",
+    size: 200,
+  },
 
-    {
-        header: "Award Amount (NGN)",
-        id: "amount_ngn",
-        accessorKey: "amount_ngn",
-        size: 200,
-    },
+  {
+    header: "Award Amount (NGN)",
+    id: "amount_ngn",
+    accessorKey: "amount_ngn",
+    accessorFn: (data) => formatNumberCurrency(data.amount_ngn, "NGN"),
 
-    {
-        header: "Start Date",
-        accessorKey: "start_date",
-        size: 200,
-    },
-    {
-        header: "End Date",
-        accessorKey: "end_date",
-        size: 200,
-    },
-    {
-        header: "Status",
-        accessorKey: "status",
-        size: 200,
-    },
-    {
-        header: "",
-        id: "actions",
-        size: 50,
-        cell: ({ row }) => <TableMenu {...row.original} />,
-    },
+    size: 200,
+  },
+
+  {
+    header: "Start Date",
+    accessorKey: "start_date",
+    size: 200,
+  },
+  {
+    header: "End Date",
+    accessorKey: "end_date",
+    size: 200,
+  },
+  {
+    header: "Status",
+    accessorKey: "status",
+    size: 200,
+  },
+  {
+    header: "",
+    id: "actions",
+    size: 50,
+    cell: ({ row }) => <TableMenu {...row.original} />,
+  },
 ];
 
 const TableMenu = ({ id }: ISubGrantPaginatedData) => {
-    const [isDialogOpen, setDialogOpen] = useState(false);
+  const [isDialogOpen, setDialogOpen] = useState(false);
 
-    const [deleteSubGrant, { isLoading: isDeleteLoading }] =
-        useDeleteSubGrantMutation();
+  const [deleteSubGrant, { isLoading: isDeleteLoading }] =
+    useDeleteSubGrantMutation();
 
-    const handleDelete = async () => {
-        try {
-            await deleteSubGrant(id).unwrap();
-            toast.success("Sub Grant Deleted");
-            setDialogOpen(false);
-        } catch (error: any) {
-            toast.error(error.data.message ?? "Something went wrong");
-        }
-    };
+  const handleDelete = async () => {
+    try {
+      await deleteSubGrant(id).unwrap();
+      toast.success("Sub Grant Deleted");
+      setDialogOpen(false);
+    } catch (error: any) {
+      toast.error(error.data.message ?? "Something went wrong");
+    }
+  };
 
-    return (
-        <div className="flex items-center gap-2">
-            <>
-                <Popover>
-                    <PopoverTrigger asChild>
-                        <Button variant="ghost" className="flex gap-2 py-6">
-                            <MoreOptionsHorizontalIcon />
-                        </Button>
-                    </PopoverTrigger>
-                    <PopoverContent className="w-fit">
-                        <Link
-                            to={generatePath(CG_ROUTES.SUBGRANT_AWARD_DETAILS, {
-                                id,
-                            })}
-                        >
-                            <Button
-                                className="w-full flex items-center justify-start gap-2"
-                                variant="ghost"
-                            >
-                                <EyeIcon />
-                                View
-                            </Button>
-                        </Link>
-                        <Link
-                            to={{
-                                pathname: CG_ROUTES.CREATE_SUBGRANT_AWARD,
-                                search: `?id=${id}`,
-                            }}
-                        >
-                            <Button
-                                className="w-full flex items-center justify-start gap-2"
-                                variant="ghost"
-                            >
-                                <PencilIcon />
-                                Edit
-                            </Button>
-                        </Link>
-                        <Button
-                            className="w-full flex items-center justify-start gap-2"
-                            variant="ghost"
-                            onClick={() => setDialogOpen(true)}
-                        >
-                            <DeleteIcon />
-                            Delete
-                        </Button>
-                    </PopoverContent>
-                </Popover>
-            </>
+  return (
+    <div className="flex items-center gap-2">
+      <>
+        <Popover>
+          <PopoverTrigger asChild>
+            <Button variant="ghost" className="flex gap-2 py-6">
+              <MoreOptionsHorizontalIcon />
+            </Button>
+          </PopoverTrigger>
+          <PopoverContent className="w-fit">
+            <Link
+              to={generatePath(CG_ROUTES.SUBGRANT_AWARD_DETAILS, {
+                id,
+              })}
+            >
+              <Button
+                className="w-full flex items-center justify-start gap-2"
+                variant="ghost"
+              >
+                <EyeIcon />
+                View
+              </Button>
+            </Link>
+            <Link
+              to={{
+                pathname: CG_ROUTES.CREATE_SUBGRANT_AWARD,
+                search: `?id=${id}`,
+              }}
+            >
+              <Button
+                className="w-full flex items-center justify-start gap-2"
+                variant="ghost"
+              >
+                <PencilIcon />
+                Edit
+              </Button>
+            </Link>
+            <Button
+              className="w-full flex items-center justify-start gap-2"
+              variant="ghost"
+              onClick={() => setDialogOpen(true)}
+            >
+              <DeleteIcon />
+              Delete
+            </Button>
+          </PopoverContent>
+        </Popover>
+      </>
 
-            <ConfirmationDialog
-                open={isDialogOpen}
-                title="Are you sure you want to delete this sub grant?"
-                loading={isDeleteLoading}
-                onCancel={() => setDialogOpen(false)}
-                onOk={handleDelete}
-            />
-        </div>
-    );
+      <ConfirmationDialog
+        open={isDialogOpen}
+        title="Are you sure you want to delete this sub grant?"
+        loading={isDeleteLoading}
+        onCancel={() => setDialogOpen(false)}
+        onOk={handleDelete}
+      />
+    </div>
+  );
 };
