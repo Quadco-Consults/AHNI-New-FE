30d0da2a46f66dda0c16964159f5310004b401dc
thecoderhoc <ubakawilson@gmail.com>
2025-04-29 08:58:52 +0100

    Working on the ssp evaluation

--- src/definations/program/plan/supervision-plan/supervision-plan-review.ts
+++ src/definations/program/plan/supervision-plan/supervision-plan-review.ts
@@ -1,3 +1,5 @@
+import { IUser } from "definations/auth/user";
+import { TSolicitationEvaluationCriteriaData } from "definations/modules/procurement/solicitation-evaluation-criteria";
 import { z } from "zod";
 
 export const SupervisionPlanReviewSchema = z.object({
@@ -29,6 +31,29 @@ export type TSupervisionPlanReviewFormData = z.infer<
     typeof SupervisionPlanReviewSchema
 >;
 
-export interface ISupervisionPlanReviewPaginatedData {}
+export interface ISupervisionPlanReviewPaginatedData {
+    id: string;
+    reviews: {
+        id: string;
+        objective: TSolicitationEvaluationCriteriaData;
+        created_datetime: string;
+        updated_datetime: string;
+        is_selected: true;
+        comment: string;
+    }[];
+    documents: {
+        id: string;
+        created_datetime: string;
+        updated_datetime: string;
+        document: string;
+        is_selected: boolean;
+        title: string;
+    }[];
+    user: IUser;
+    submission_datetime: string;
+    remediation_plan: string;
+    is_agree_on_visit_plan: boolean;
+    supportive_supervision_plan: string;
+}
 
 export interface ISupervisionPlanReviewSingleData {}
