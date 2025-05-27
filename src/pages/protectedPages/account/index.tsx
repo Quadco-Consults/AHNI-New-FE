5b6d481b55fc47432d43242ea3a9ce0c34d575c1
isaac-jamezs <isaac.j@pashione.co>
2025-05-27 07:43:36 +0100

    `feat: updated account settings page with user profile and security form validation`

--- src/pages/protectedPages/account/index.tsx
+++ src/pages/protectedPages/account/index.tsx
@@ -1,33 +1,45 @@
 import Card from "components/shared/Card";
-import { Button } from "components/ui/button";
-import SearchIcon from "components/icons/SearchIcon";
-import FilterIcon from "components/icons/FilterIcon";
+// import { Button } from "components/ui/button";
+// import SearchIcon from "components/icons/SearchIcon";
+// import FilterIcon from "components/icons/FilterIcon";
 import {
   Breadcrumb,
   BreadcrumbItem,
   BreadcrumbList,
   BreadcrumbPage,
-  BreadcrumbSeparator,
+  // BreadcrumbSeparator,
 } from "components/ui/breadcrumb";
-import { useAppDispatch } from "hooks/useStore";
+// import { useAppDispatch } from "hooks/useStore";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "components/ui/tabs";
 import { z } from "zod";
 import {
   ProfileSchema,
   SecuritySchema,
-  TProfileFormValues,
-  TSecurityFormValues,
+  // TProfileFormValues,
+  // TSecurityFormValues,
 } from "definations/account/account";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { FormProvider, useForm } from "react-hook-form";
 import FormInput from "atoms/FormInput";
 import FormButton from "atoms/FormButton";
 import { ImagePlus } from "lucide-react";
-import { ChangeEvent, useRef, useState } from "react";
+import { ChangeEvent, useEffect, useRef, useState } from "react";
+import {
+  useGetUserProfileQuery,
+  useUpdateUserMutation,
+} from "services/auth/user";
+import { toast } from "sonner";
 
 export type TFormValues = z.infer<typeof ProfileSchema>;
 export type TFormValuesSecond = z.infer<typeof SecuritySchema>;
 export default function Account() {
+  // const dispatch = useAppDispatch();
+
+  const { data: profile } = useGetUserProfileQuery(null);
+
+  const [updateUser, { isLoading: isUpdateLoading }] = useUpdateUserMutation();
+  console.log({ profile });
+
   const Profileform = useForm<TFormValues>({
     resolver: zodResolver(ProfileSchema),
     defaultValues: {
@@ -36,15 +48,38 @@ export default function Account() {
       email: "",
       username: "",
       role: "",
+      gender: "",
     },
   });
   const Securityform = useForm<TFormValuesSecond>({
     resolver: zodResolver(SecuritySchema),
-    defaultValues: {},
+    // defaultValues: {},
+    defaultValues: {
+      old_password: "",
+      new_password: "",
+      confirm_password: "",
+    },
   });
-  const dispatch = useAppDispatch();
   const [file, setFile] = useState<File>();
   const fileInputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    if (profile?.data) {
+      Profileform.reset({
+        first_name: profile.data.first_name || "",
+        last_name: profile.data.last_name || "",
+        email: profile.data.email || "",
+        // @ts-ignore
+        username: profile.data.username || "",
+        // @ts-ignore
+        role: profile.data.role || "",
+        gender: profile.data.gender || "",
+      });
+    }
+  }, [profile, Profileform]);
+
+  const vn = Profileform.getValues();
+  console.log({ vn });
 
   const handlePaperclipClick = () => {
     if (fileInputRef.current) {
@@ -55,6 +90,26 @@ export default function Account() {
     if (e.target.files) {
       setFile(e.target.files[0]);
     }
+  };
+
+  const onSubmitProfile = async (data: TFormValues) => {
+    // Dispatch update profile action or API call
+    console.log("Profile Data Submitted:", data);
+
+    try {
+      await updateUser({
+        id: profile?.data?.id as string,
+        body: data,
+      }).unwrap();
+      toast.success("User Updated");
+    } catch (error: any) {
+      toast.error(error.data.message ?? "Something went wrong");
+    }
+  };
+
+  const onSubmitSecurity = (data: TFormValuesSecond) => {
+    // Dispatch update password action or API call
+    console.log("Security Data Submitted:", data);
   };
 
   return (
@@ -71,7 +126,16 @@ export default function Account() {
         <h2 className='text-[28px] font-semibold text-[#101928]'>Settings</h2>
         <div className='w-full grid grid-cols-1 lg:grid-cols-4 py-16 lg:py-36 gap-3'>
           <div className='col-span-1 py-20   px-2 flex flex-col items-center gap-3'>
-            <div className='w-[200px] h-[200px] flex-shrink-0 rounded-full bg-[#FF0000]'></div>
+            <div
+              className='w-[200px] h-[200px] flex-shrink-0 rounded-full bg-[#FF0000]'
+              style={{
+                backgroundImage: file
+                  ? `url(${URL.createObjectURL(file)})`
+                  : undefined,
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+              }}
+            />
             <div
               onClick={handlePaperclipClick}
               className='rounded-[8px] cursor-pointer border-[1.5px] w-fit p-2 flex justify-center items-center gap-1 border-[#FF0000] text-[#FF0000] text-[14px] font-semibold'
@@ -108,7 +172,7 @@ export default function Account() {
               >
                 <FormProvider {...Profileform}>
                   <form
-                    /*  onSubmit={handleSubmit(onSubmit)} */
+                    onSubmit={Profileform.handleSubmit(onSubmitProfile)}
                     className='flex flex-col w-full gap-[20px]'
                   >
                     <div className='w-full grid grid-cols-1 lg:grid-cols-2 gap-[20px]'>
@@ -133,28 +197,29 @@ export default function Account() {
                       className='bg-white h-[56px]'
                       required
                       placeholder='Enter Email address'
+                      disabled={true}
                     />
-                    <FormInput
+                    {/* <FormInput
                       label='Username'
                       name='username'
                       className='bg-white h-[56px]'
                       required
                       placeholder='Enter username'
-                    />
+                    /> */}
 
                     <FormInput
                       label='Role'
                       name='role'
                       className='bg-white h-[56px]'
-                      disabled
-                      placeholder='Enter role'
+                      // disabled
+                      placeholder='Role'
                     />
 
                     <div className='flex justify-end gap-5 mt-16'>
                       <FormButton
-                        loading={false}
+                        loading={isUpdateLoading}
                         type='submit'
-                        disabled={false}
+                        disabled={isUpdateLoading}
                       >
                         Save
                       </FormButton>
@@ -172,7 +237,7 @@ export default function Account() {
                   </h2>
                   <FormProvider {...Securityform}>
                     <form
-                      /*  onSubmit={handleSubmit(onSubmit)} */
+                      onSubmit={Securityform.handleSubmit(onSubmitSecurity)}
                       className='flex flex-col w-full gap-[20px]'
                     >
                       <FormInput
@@ -181,7 +246,7 @@ export default function Account() {
                         name='old_password'
                         className='bg-white h-[56px]'
                         required
-                        placeholder='Enter Email address'
+                        placeholder='Enter old password'
                       />
                       <FormInput
                         label='New Password'
@@ -189,7 +254,7 @@ export default function Account() {
                         name='new_password'
                         className='bg-white h-[56px]'
                         required
-                        placeholder='Enter username'
+                        placeholder='Enter new password'
                       />
 
                       <FormInput
@@ -197,8 +262,8 @@ export default function Account() {
                         type='password'
                         name='confirm_password'
                         className='bg-white h-[56px]'
-                        disabled
-                        placeholder='Enter role'
+                        // disabled
+                        placeholder='Confirm new password'
                       />
 
                       <div className='flex justify-end gap-5 mt-16'>
