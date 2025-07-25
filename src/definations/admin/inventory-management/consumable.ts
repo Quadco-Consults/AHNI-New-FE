1b3e10cdb062ad2a2b2b72fe2c720d79222cd229
isaac-jamezs <isaac.j@pashione.co>
2025-07-25 09:13:40 +0100

    `Refactored code in various files for inventory management and procurement management features`

--- src/definations/admin/inventory-management/consumable.ts
+++ src/definations/admin/inventory-management/consumable.ts
@@ -3,72 +3,73 @@ import { TItemData } from "definations/modules/config/item";
 import { z } from "zod";
 
 export const ConsumableSchema = z.object({
-    name: z.string().min(1, "Please enter an item name"),
-    description: z.string().min(1, "Please enter a description"),
+  name: z.string().min(1, "Please enter an item name"),
+  description: z.string().min(1, "Please enter a description"),
+  uom: z.string().min(1, "Please enter a uom"),
+  category: z.string().min(1, "Please select category"),
 });
 
 export const EditConsumableSchema = z.object({
-    name: z.string().min(1, "Please enter an item name"),
-    description: z.string().min(1, "Please enter a description"),
-    quantity: z.string().min(1, "Please enter quantity"),
-    stock_control_method: z
-        .string()
-        .min(1, "Please select stock control method"),
-    category: z.string().min(1, "Please select category"),
-    expiry_date: z.string().min(1, "Please select expiry date"),
-    previous_quantity: z.string().min(1, "Please enter previous quantity"),
-    re_order_level: z.string().min(1, "Please enter re-order levek"),
-    buffer_stock: z.string().min(1, "Please enter buffer stock"),
-    max_stock: z.string().min(1, "Please enter max stock"),
-    entry_date: z.string().min(1, "Please select entry date"),
-    available_quantity: z.string().min(1, "Please enter available quantity"),
-    item_cost: z.string().min(1, "Please enter item cost"),
-    grn_tracking_number: z.string().optional(),
+  name: z.string().min(1, "Please enter an item name"),
+  description: z.string().min(1, "Please enter a description"),
+  uom: z.string().min(1, "Please enter a uom"),
+  quantity: z.string().min(1, "Please enter quantity"),
+  stock_control_method: z.string().min(1, "Please select stock control method"),
+  category: z.string().min(1, "Please select category"),
+  expiry_date: z.string().min(1, "Please select expiry date"),
+  previous_quantity: z.string().min(1, "Please enter previous quantity"),
+  re_order_level: z.string().min(1, "Please enter re-order levek"),
+  buffer_stock: z.string().min(1, "Please enter buffer stock"),
+  max_stock: z.string().min(1, "Please enter max stock"),
+  entry_date: z.string().min(1, "Please select entry date"),
+  available_quantity: z.string().min(1, "Please enter available quantity"),
+  item_cost: z.string().min(1, "Please enter item cost"),
+  grn_tracking_number: z.string().optional(),
 });
 
 export type TConsumableFormValues = z.infer<typeof ConsumableSchema>;
 
 export interface TConsumablePaginatedData {
-    id: string;
-    name: string;
-    description: string;
-    category: string;
-    created_datetime: string;
-    updated_datetime: string;
-    quantity: number;
-    stock_control_method: string;
-    expiry_date: string;
-    previous_quantity: number;
-    re_order_level: number;
-    buffer_stock: number;
-    max_stock: number;
-    entry_date: string;
-    available_quantity: number;
-    item_cost: string;
-    grn_tracking_number: null;
-    created_by: string;
-    updated_by: null;
-    item: TItemData;
+  id: string;
+  name: string;
+  description: string;
+  category: string;
+  created_datetime: string;
+  updated_datetime: string;
+  quantity: number;
+  stock_control_method: string;
+  expiry_date: string;
+  previous_quantity: number;
+  re_order_level: number;
+  buffer_stock: number;
+  max_stock: number;
+  entry_date: string;
+  available_quantity: number;
+  item_cost: string;
+  grn_tracking_number: null;
+  created_by: string;
+  updated_by: null;
+  item: TItemData;
 }
 
 export interface TConsumableSingleData {
-    id: string;
-    name: string;
-    description: string;
-    category: TCategoryData;
-    created_datetime: string;
-    updated_datetime: string;
-    quantity: number;
-    stock_control_method: string;
-    expiry_date: string;
-    previous_quantity: number;
-    re_order_level: number;
-    buffer_stock: number;
-    max_stock: number;
-    entry_date: string;
-    available_quantity: number;
-    item_cost: string;
-    grn_tracking_number: string;
-    created_by: string;
-    updated_by: null;
+  id: string;
+  name: string;
+  description: string;
+  category: TCategoryData;
+  created_datetime: string;
+  updated_datetime: string;
+  quantity: number;
+  stock_control_method: string;
+  expiry_date: string;
+  previous_quantity: number;
+  re_order_level: number;
+  buffer_stock: number;
+  max_stock: number;
+  entry_date: string;
+  available_quantity: number;
+  item_cost: string;
+  grn_tracking_number: string;
+  created_by: string;
+  updated_by: null;
 }
--- src/pages/protectedPages/admin/inventory-management/consumable/index.tsx
+++ src/pages/protectedPages/admin/inventory-management/consumable/index.tsx
@@ -1,65 +1,50 @@
 import { useState } from "react";
 import Card from "components/shared/Card";
 import { Button } from "components/ui/button";
-import { FileDown, Plus } from "lucide-react";
+import { Plus } from "lucide-react";
 import { Link, generatePath } from "react-router-dom";
 import { AdminRoutes } from "constants/RouterConstants";
 import DataTable from "components/Table/DataTable";
 import { consumableColums } from "components/Table/columns/admin/inventory-management/consumables";
 import TableFilters from "components/Table/TableFilters";
-import { UploadFileSvg } from "assets/svgs/CAndGSvgs";
-import { useGetAllConsumablesQuery } from "services/admin/inventory-management/consumable";
+import { useGetAllItemsQuery } from "services/modules/config/item";
 
 export default function ConsumablesHomePage() {
-    const [page, setPage] = useState(1);
+  const [page, setPage] = useState(1);
 
-    const { data: consumable, isFetching } = useGetAllConsumablesQuery({
-        page,
-        size: 10,
-    });
+  const { data: item, isFetching } = useGetAllItemsQuery({
+    page,
+    size: 20,
+    category: "b3fb7e09-deb2-4feb-b6d6-66595747350a",
+  });
 
-    return (
-        <div className="space-y-10">
-            <Card className="space-y-10">
-                <div className="space-y-5">
-                    <div className="flex justify-end">
-                        <Link to={generatePath(AdminRoutes.CREATE_CONSUMABLE)}>
-                            <Button>
-                                <Plus size={20} />
-                                Add Consumable
-                            </Button>
-                        </Link>
-                    </div>
-
-                    {/* <div className="flex gap-x-4 justify-end">
-                        <Button variant="outline">
-                            <span>
-                                <UploadFileSvg />
-                            </span>
-                            Upload
-                        </Button>
-                        <Button variant="custom">
-                            <span>
-                                <FileDown size={18} />
-                            </span>
-                            Download
-                        </Button>
-                    </div> */}
-                </div>
-                <TableFilters>
-                    <DataTable
-                        data={consumable?.data.results || []}
-                        isLoading={isFetching}
-                        columns={consumableColums}
-                        pagination={{
-                            total: consumable?.data.pagination.count ?? 0,
-                            pageSize:
-                                consumable?.data.pagination.page_size ?? 0,
-                            onChange: (page: number) => setPage(page),
-                        }}
-                    />
-                </TableFilters>
-            </Card>
+  return (
+    <div className='space-y-10'>
+      <Card className='space-y-10'>
+        <div className='space-y-5'>
+          <div className='flex justify-end'>
+            <Link to={generatePath(AdminRoutes.CREATE_CONSUMABLE)}>
+              <Button>
+                <Plus size={20} />
+                Add Consumable
+              </Button>
+            </Link>
+          </div>
         </div>
-    );
+        <TableFilters>
+          <DataTable
+            data={item?.data.results || []}
+            isLoading={isFetching}
+            // @ts-ignore
+            columns={consumableColums}
+            pagination={{
+              total: item?.data.pagination.count ?? 0,
+              pageSize: item?.data.pagination.page_size ?? 0,
+              onChange: (page: number) => setPage(page),
+            }}
+          />
+        </TableFilters>
+      </Card>
+    </div>
+  );
 }
