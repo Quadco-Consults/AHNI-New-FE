59e457fa6a3986f2c1ef887bb18f7753f716d7ec
isaac-jamezs <isaac.j@pashione.co>
2025-08-25 14:31:12 +0100

    `Update Uploads import to use dynamic import with loading indicator`

--- src/app/dashboard/projects/create/uploads/page.tsx
+++ src/app/dashboard/projects/create/uploads/page.tsx
@@ -1,6 +1,11 @@
 "use client";
 
-import Uploads from "@/features/projects/components/projects/create/Uploads";
+import dynamic from "next/dynamic";
+
+const Uploads = dynamic(() => import("@/features/projects/components/projects/create/Uploads"), {
+  ssr: false,
+  loading: () => <div>Loading...</div>
+});
 
 export default function ProjectUploadsPage() {
     return <Uploads />;
