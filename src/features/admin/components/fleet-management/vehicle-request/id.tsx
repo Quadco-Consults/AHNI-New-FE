0c909b1b8a4ac0b10b10a47ac607964d04f238ca
isaac-jamezs <isaac.j@pashione.co>
2025-09-04 10:04:08 +0100

    `feat: Update Vehicle Request Details component and related controllers for approval feature`
    `src/features/admin/components/fleet-management/vehicle-request/id.tsx`: Update component to handle approval feature
    
    `src/features/admin/controllers/assetController.ts`: Update query key

--- src/features/admin/components/fleet-management/vehicle-request/id.tsx
+++ src/features/admin/components/fleet-management/vehicle-request/id.tsx
@@ -11,195 +11,258 @@ import { Button } from "components/ui/button";
 import { Card } from "components/ui/card";
 import { format } from "date-fns";
 import { PlusIcon } from "lucide-react";
-import { useMemo } from "react";
-import { FormProvider, useForm } from "react-hook-form";
-import { useParams } from "next/navigation";
-import { useGetSingleVehicleRequestQuery } from "@/features/admin/controllers/vehicleRequestController";
-import { useGetAllAssetsQuery } from "@/features/admin/controllers/assetController";
+import { useMemo, useEffect } from "react";
+import { FormProvider, useForm, useFieldArray } from "react-hook-form";
+import { useParams, useRouter } from "next/navigation";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { toast } from "sonner";
+import {
+  useGetSingleVehicleRequestQuery,
+  useApproveVehicleRequestMutation,
+} from "@/features/admin/controllers/vehicleRequestController";
 import { useGetAllUsersQuery } from "@/features/auth/controllers/userController";
+import {
+  VehicleRequestApprovalSchema,
+  TVehicleRequestApprovalFormValues,
+} from "@/features/admin/types/fleet-management/vehicle-request";
+import { useGetAllItemsQuery } from "@/features/modules/controllers";
 
 export default function VehicleRequestDetails() {
-    const { id } = useParams();
+  const params = useParams();
+  const id = params?.id as string;
+  const router = useRouter();
 
-    const { data, isLoading } = useGetSingleVehicleRequestQuery(
-        id || "", !!id
-    );
+  const { data, isLoading } = useGetSingleVehicleRequestQuery(
+    id as string,
+    !!id
+  );
 
-    const form = useForm();
+  const {
+    approveVehicleRequest,
+    isLoading: isApproving,
+    isSuccess,
+  } = useApproveVehicleRequestMutation(id as string);
 
-    const { data: vehicle } = useGetAllAssetsQuery({ page: 1, size: 2000000 });
+  const form = useForm<TVehicleRequestApprovalFormValues>({
+    resolver: zodResolver(VehicleRequestApprovalSchema),
+    defaultValues: {
+      vehicles: [{ vehicle: "", driver: "" }],
+      comment: "",
+    },
+  });
 
-    const vehicleOptions = useMemo(
-        () =>
-            vehicle?.data.results.map(({ name, id }) => ({
-                label: name,
-                value: id,
-            })),
-        [vehicle]
-    );
+  const { fields, append, remove } = useFieldArray({
+    control: form.control,
+    name: "vehicles",
+  });
 
-    const { data: driver } = useGetAllUsersQuery({ page: 1, size: 2000000 });
+  // Fetch vehicles with category filter for "vehicle"
+  const { data: vehicleData } = useGetAllItemsQuery({
+    page: 1,
+    size: 2000000,
+    category: "b0983944-f926-4141-8e28-093960d75246",
+  });
 
-    const driverOptions = useMemo(
-        () =>
-            driver?.data.results.map(({ first_name, last_name, id }) => ({
-                label: `${first_name} ${last_name}`,
-                value: id,
-            })),
-        [driver]
-    );
+  const vehicleOptions = useMemo(
+    () =>
+      vehicleData?.data?.results?.map((item: any) => ({
+        label: item.name,
+        value: item.id,
+      })) || [],
+    [vehicleData]
+  );
 
-    return (
-        <div className="space-y-4">
-            <BackNavigation extraText="View Vehicle Request" />
-            <Card className="p-6 mx-auto space-y-5 ">
-                {isLoading ? (
-                    <LoadingSpinner />
-                ) : (
-                    data && (
-                        <>
-                            <DescriptionCard
-                                label="Requesting Staff"
-                                description={`${
-                                    data?.data?.requesting_staff?.first_name ||
-                                    ""
-                                } ${
-                                    data?.data?.requesting_staff?.last_name ||
-                                    ""
-                                }`}
-                            />
+  // Fetch drivers (users)
+  const { data: driverData } = useGetAllUsersQuery({ page: 1, size: 2000000 });
 
-                            <div className="grid grid-cols-3 gap-5 mb-6">
-                                <DescriptionCard
-                                    label="Location"
-                                    description={data?.data.location.name}
-                                />
+  const driverOptions = useMemo(
+    () =>
+      driverData?.data?.results?.map((user: any) => ({
+        label: `${user.first_name} ${user.last_name}`,
+        value: user.id,
+      })) || [],
+    [driverData]
+  );
 
-                                <DescriptionCard
-                                    label="Date of Request"
-                                    description={format(
-                                        data?.data.created_datetime,
-                                        "dd-MMM-yyyy"
-                                    )}
-                                />
+  const onSubmit = async (formData: TVehicleRequestApprovalFormValues) => {
+    try {
+      // Send vehicle-driver pairs directly as expected by backend
+      const payload = {
+        vehicles: formData.vehicles, // Array of {vehicle: "id", driver: "id"} objects
+        comment: formData.comment
+      };
+      
+      await approveVehicleRequest(payload);
+    } catch (error) {
+      console.error("Approval failed:", error);
+      toast.error("Failed to approve vehicle request. Please try again.");
+    }
+  };
 
-                                <DescriptionCard
-                                    label="Travel Destination"
-                                    description={data?.data.travel_destination}
-                                />
+  const addVehicle = () => {
+    append({ vehicle: "", driver: "" });
+  };
 
-                                <DescriptionCard
-                                    label="Departure Date"
-                                    description={format(
-                                        data?.data.departure_datetime,
-                                        "dd-MMM-yyyy"
-                                    )}
-                                />
+  const removeVehicle = (index: number) => {
+    if (fields.length > 1) {
+      remove(index);
+    }
+  };
 
-                                <DescriptionCard
-                                    label="Return Date"
-                                    description={format(
-                                        data?.data.return_datetime,
-                                        "dd-MMM-yyyy"
-                                    )}
-                                />
+  // Handle success
+  useEffect(() => {
+    if (isSuccess) {
+      toast.success("Vehicle request approved successfully!");
+      router.push("/dashboard/admin/fleet-management/vehicle-request");
+    }
+  }, [isSuccess, router]);
 
-                                <DescriptionCard
-                                    label="Point of Departure"
-                                    description={data?.data.departure_point}
-                                />
-                            </div>
+  return (
+    <div className='space-y-4'>
+      <BackNavigation extraText='View Vehicle Request' />
+      <Card className='p-6 mx-auto space-y-5'>
+        {isLoading ? (
+          <LoadingSpinner />
+        ) : (
+          data && (
+            <>
+              <DescriptionCard
+                label='Requesting Staff'
+                description={`${
+                  data?.data?.requesting_staff?.first_name || ""
+                } ${data?.data?.requesting_staff?.last_name || ""}`}
+              />
 
-                            <h3 className="mb-2 text-lg font-bold text-yellow-500">
-                                Travel Team Members (
-                                {data?.data.travel_team_members.length})
-                            </h3>
-                            <div className="grid grid-cols-4 gap-4 mb-6">
-                                {data?.data.travel_team_members.map(
-                                    ({
-                                        id,
-                                        first_name,
-                                        last_name,
-                                        position,
-                                        mobile_number,
-                                    }) => (
-                                        <Card
-                                            key={id}
-                                            className="p-2 space-y-3 bg-amber-50"
-                                        >
-                                            <p>
-                                                <span className="font-bold">
-                                                    Name:&nbsp;
-                                                </span>
-                                                {first_name}&nbsp;
-                                                {last_name}
-                                            </p>
-                                            <p>
-                                                <span className="font-bold">
-                                                    Position:
-                                                </span>
-                                                &nbsp;
-                                                {position || "N/A"}
-                                            </p>
-                                            <p>
-                                                <span className="font-bold">
-                                                    Tel:&nbsp;
-                                                </span>
-                                                {mobile_number || "N/A"}
-                                            </p>
-                                        </Card>
-                                    )
-                                )}
-                            </div>
+              <div className='grid grid-cols-3 gap-5 mb-6'>
+                <DescriptionCard
+                  label='Location'
+                  description={data?.data.location.name}
+                />
 
-                            <FormProvider {...form}>
-                                <form className="space-y-5">
-                                    <h3 className="mb-2 text-lg font-bold">
-                                        Vehicle
-                                    </h3>
+                <DescriptionCard
+                  label='Date of Request'
+                  description={format(
+                    data?.data.created_datetime,
+                    "dd-MMM-yyyy"
+                  )}
+                />
 
-                                    <div className="flex items-center gap-5">
-                                        <FormSelect
-                                            name=""
-                                            className="flex-1"
-                                            placeholder="Select Vehicle"
-                                            options={vehicleOptions}
-                                        />
+                <DescriptionCard
+                  label='Travel Destination'
+                  description={data?.data.travel_destination}
+                />
 
-                                        <FormSelect
-                                            name=""
-                                            className="flex-1"
-                                            placeholder="Select Driver"
-                                            options={driverOptions}
-                                        />
+                <DescriptionCard
+                  label='Departure Date'
+                  description={format(
+                    data?.data.departure_datetime,
+                    "dd-MMM-yyyy"
+                  )}
+                />
 
-                                        <Button variant="ghost">
-                                            <DeleteIcon />
-                                        </Button>
-                                    </div>
+                <DescriptionCard
+                  label='Return Date'
+                  description={format(
+                    data?.data.return_datetime,
+                    "dd-MMM-yyyy"
+                  )}
+                />
 
-                                    <Button type="button">
-                                        Add Vehicle <PlusIcon />
-                                    </Button>
+                <DescriptionCard
+                  label='Point of Departure'
+                  description={data?.data.departure_point}
+                />
+              </div>
 
-                                    <FormTextArea
-                                        label="Comment"
-                                        name="comment"
-                                        placeholder="Enter Comment"
-                                    />
-
-                                    <FormButton
-                                        size="lg"
-                                        className="bg-green-500"
-                                    >
-                                        Approve Request
-                                    </FormButton>
-                                </form>
-                            </FormProvider>
-                        </>
-                    )
+              <h3 className='mb-2 text-lg font-bold text-yellow-500'>
+                Travel Team Members ({data?.data.travel_team_members.length})
+              </h3>
+              <div className='grid grid-cols-4 gap-4 mb-6'>
+                {data?.data.travel_team_members.map(
+                  ({ id, first_name, last_name, position, mobile_number }) => (
+                    <Card key={id} className='p-2 space-y-3 bg-amber-50'>
+                      <p>
+                        <span className='font-bold'>Name:&nbsp;</span>
+                        {first_name}&nbsp;
+                        {last_name}
+                      </p>
+                      <p>
+                        <span className='font-bold'>Position:</span>
+                        &nbsp;
+                        {position || "N/A"}
+                      </p>
+                      <p>
+                        <span className='font-bold'>Tel:&nbsp;</span>
+                        {mobile_number || "N/A"}
+                      </p>
+                    </Card>
+                  )
                 )}
-            </Card>
-        </div>
-    );
+              </div>
+
+              <FormProvider {...form}>
+                <form
+                  onSubmit={form.handleSubmit(onSubmit)}
+                  className='space-y-5'
+                >
+                  <h3 className='mb-2 text-lg font-bold'>
+                    Vehicles & Drivers
+                  </h3>
+
+                  {fields.map((field, index) => (
+                    <div key={field.id} className='flex items-center gap-5'>
+                      <FormSelect
+                        name={`vehicles.${index}.vehicle`}
+                        className='flex-1'
+                        placeholder='Select Vehicle'
+                        options={vehicleOptions}
+                        label={index === 0 ? "Vehicle" : ""}
+                      />
+
+                      <FormSelect
+                        name={`vehicles.${index}.driver`}
+                        className='flex-1'
+                        placeholder='Select Driver'
+                        options={driverOptions}
+                        label={index === 0 ? "Driver" : ""}
+                      />
+
+                      <Button
+                        type='button'
+                        variant='ghost'
+                        onClick={() => removeVehicle(index)}
+                        disabled={fields.length === 1}
+                        className='mt-6'
+                      >
+                        <DeleteIcon />
+                      </Button>
+                    </div>
+                  ))}
+
+                  <Button type='button' onClick={addVehicle}>
+                    Add Vehicle <PlusIcon />
+                  </Button>
+
+                  <FormTextArea
+                    label='Comment'
+                    name='comment'
+                    placeholder='Enter Comment (Optional)'
+                  />
+
+                  <FormButton
+                    size='lg'
+                    className='bg-green-500'
+                    loading={isApproving}
+                    disabled={isApproving}
+                  >
+                    {isApproving ? "Approving..." : "Approve Request"}
+                  </FormButton>
+                </form>
+              </FormProvider>
+            </>
+          )
+        )}
+      </Card>
+    </div>
+  );
 }
