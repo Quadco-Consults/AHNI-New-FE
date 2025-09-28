26ed9ab3f7c3ce24a37d4aa15cb9938765ad642f
gidado <gidado.developer@gmail.com>
2025-09-28 17:09:02 +0100

    sub grant advert

---
+++ src/app/dashboard/c-and-g/sub-grant/awards/[id]/submission/create/page.tsx
@@ -0,0 +1,5 @@
+import CreateSubGrantSubDetails from "@/features/contracts-grants/components/sub-grant/awards/id/submission/create";
+
+export default function CreateSubGrantSubmissionPage() {
+    return <CreateSubGrantSubDetails />;
+}
