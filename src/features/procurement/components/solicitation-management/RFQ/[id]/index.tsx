a1b4ddab03cee1bf145c87111bdcbf32420ed55b
Muhammad Ilu <ilu.muhammad@gmail.com>
2025-09-29 02:00:25 +0100

    Pr 2

--- src/features/procurement/components/solicitation-management/RFQ/[id]/index.tsx
+++ src/features/procurement/components/solicitation-management/RFQ/[id]/index.tsx
@@ -107,9 +107,12 @@ const RFQDetails = () => {
         <TabsList>
           <TabsTrigger value="rfq-details">RFQ Details</TabsTrigger>
           <TabsTrigger value="vendor-submission">Vendor Submission</TabsTrigger>
-          <TabsTrigger value="vendor-submission-evaluation">
-            Vendor Submission Evaluation
-          </TabsTrigger>
+          {/* Only show vendor submission evaluation tab for EOI national open tenders */}
+          {isFromEOI && (
+            <TabsTrigger value="vendor-submission-evaluation">
+              Vendor Submission Evaluation
+            </TabsTrigger>
+          )}
         </TabsList>
         <TabsContent value="rfq-details">
           {data && <DetailsContent {...data?.data} />}
@@ -119,9 +122,12 @@ const RFQDetails = () => {
           {data && <VendorSubmission {...data?.data} />}
         </TabsContent>
 
-        <TabsContent value="vendor-submission-evaluation">
-          <SummaryOfTechnicalPrequalification solicitationId={id as string} />
-        </TabsContent>
+        {/* Only render vendor submission evaluation content for EOI national open tenders */}
+        {isFromEOI && (
+          <TabsContent value="vendor-submission-evaluation">
+            <SummaryOfTechnicalPrequalification solicitationId={id as string} />
+          </TabsContent>
+        )}
       </Tabs>
     </div>
   );
