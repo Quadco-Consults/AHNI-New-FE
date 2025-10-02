9d7e2ab438b8d5cf8871dacae51efdbab757c702
Muhammad Ilu <ilu.muhammad@gmail.com>
2025-10-02 15:33:26 +0100

    fix contract

---
+++ src/app/dashboard/c-and-g/consultancy/[id]/applicant/[applicantId]/contract-acceptance/page.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+import ContractAcceptance from "@/features/contracts-grants/components/contract-management/consultant-management/id/applicants/ContractAcceptance";
+
+export default function ConsultancyContractAcceptancePage() {
+    return <ContractAcceptance />;
+}
