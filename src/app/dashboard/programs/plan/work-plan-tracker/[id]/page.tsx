c5a316fec3b3ad448016e1b763d4e9f031c4e449
Muhammad Ilu <ilu.muhammad@gmail.com>
2025-10-07 00:22:00 +0100

    risk management upload

---
+++ src/app/dashboard/programs/plan/work-plan-tracker/[id]/page.tsx
@@ -0,0 +1,5 @@
+import WorkPlanTrackerDetail from "@/features/programs/components/plan/work-plan-tracker/id/index";
+
+export default function WorkPlanTrackerDetailPage() {
+  return <WorkPlanTrackerDetail />;
+}
